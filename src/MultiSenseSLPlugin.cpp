ae138404ba6a26675f4bf0b7808b75427acc27ae
Enrico Mingo <enrico.mingo@iit.it>
2016-06-13 11:25:39 +0200

    Delted comments and changed from atlasModel to robotModel

--- src/MultiSenseSLPlugin.cpp
+++ src/MultiSenseSLPlugin.cpp
@@ -66,7 +66,7 @@ MultiSenseSL::~MultiSenseSL()
 ////////////////////////////////////////////////////////////////////////////////
 void MultiSenseSL::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 {
-  this->atlasModel = _parent;
+  this->robotModel = _parent;
   this->world = _parent->GetWorld();
   this->sdf = _sdf;
 
@@ -75,18 +75,18 @@ void MultiSenseSL::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
   this->lastTime = this->world->GetSimTime();
 
   // Get imu link
-  this->imuLink = this->atlasModel->GetLink(this->imuLinkName);
+  this->imuLink = this->robotModel->GetLink(this->imuLinkName);
   if (!this->imuLink)
   {
     gzerr << this->imuLinkName << " not found\n";
-    std::vector<gazebo::physics::LinkPtr> tempLinks = this->atlasModel->GetLinks();
+    std::vector<gazebo::physics::LinkPtr> tempLinks = this->robotModel->GetLinks();
     for(int i = 0; i < tempLinks.size(); i++)
     {
       gzerr << "Available links are: " << tempLinks[i]->GetName() << "\n";
     }
   }
 
-  std::vector<gazebo::physics::JointPtr> tempJoints = this->atlasModel->GetJoints();
+  std::vector<gazebo::physics::JointPtr> tempJoints = this->robotModel->GetJoints();
   for(int i = 0; i < tempJoints.size(); i++)
   {
     gzerr << "Available Joints are: " << tempJoints[i]->GetName() << "\n";
@@ -100,16 +100,14 @@ void MultiSenseSL::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
     gzerr << "head_imu_sensor not found\n" << "\n";
 
   // \todo: add ros topic / service to reset imu (imuReferencePose, etc.)
-  //this->spindleLink = this->atlasModel->GetLink("hokuyo_link");
-  //spindle link is always the child of "hokuyo_joint", then:
-  this->spindleLink = this->atlasModel->GetJoint("hokuyo_joint")->GetChild();
+  this->spindleLink = this->robotModel->GetJoint("hokuyo_joint")->GetChild();
   if (!this->spindleLink)
   {
     gzerr << "spindle link not found, plugin will stop loading\n";
     return;
   }
 
-  this->spindleJoint = this->atlasModel->GetJoint("hokuyo_joint");
+  this->spindleJoint = this->robotModel->GetJoint("hokuyo_joint");
   if (!this->spindleJoint)
   {
     gzerr << "spindle joint not found, plugin will stop loading\n";
@@ -163,24 +161,6 @@ void MultiSenseSL::LoadThread()
 
 
   this->rosNamespace = "/multisense";
-  /**int atlasVersion; BULLSHIT
-  this->rosnode_->getParam("/atlas_version", atlasVersion);
-  if (atlasVersion == 1)
-  {
-    ROS_INFO("ros param /atlas_version == 1");
-    this->rosNamespace = "/multisense_sl";
-  }
-  else if (atlasVersion >= 3)
-  {
-    ROS_INFO("ros param /atlas_version == %d", atlasVersion);
-    this->rosNamespace = "/multisense";
-  }
-  else
-  {
-    ROS_WARN(
-        "/atlas_version not specified (1, 3, 4, or 5), assuming atlas v1.");
-    this->rosNamespace = "/multisense_sl";
-  }**/
 
   // ros publications
   // publish joint states for tf (robot state publisher)
